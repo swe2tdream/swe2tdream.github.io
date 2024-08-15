bfb0a81bed8b80070503e3b7a91654b16f5dbfcd
swe2tdream <93404963+swe2tdream@users.noreply.github.com>
2024-08-15 17:16:23 +0300

    Update labms.js

--- js/labms.js
+++ js/labms.js
@@ -1,7 +1,7 @@
 const cookies = document.cookie.split(";");
 for (cookie of cookies) {
 	const parts = cookie.split("=");
-	if (parts[1] != "0111") {
+	if (parts[1] != "2802") {
 		window.location.href = "../../learn.html"
 	}
 }
