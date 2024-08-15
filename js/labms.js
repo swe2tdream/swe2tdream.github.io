45522627de7f314be460514e5c84f1de3239f5c4
swe2tdream <93404963+swe2tdream@users.noreply.github.com>
2024-08-15 20:15:36 +0300

    Delete js directory

--- js/labms.js
+++
@@ -1,7 +0,0 @@
-const cookies = document.cookie.split(";");
-for (cookie of cookies) {
-	const parts = cookie.split("=");
-	if (parts[1] != "2802") {
-		window.location.href = "../../learn.html"
-	}
-}
--- js/prcode.js
+++
@@ -1,8 +0,0 @@
-var input = document.querySelector('input');
-
-input.oninput = function() {
-	if (input.value === "2802") {
-		document.cookie = "id=2802; max-age=86400"
-		window.location.href = "learn/labms/profile.html";
-	}
-}
