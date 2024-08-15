4db64469ab1fa28e94b4e0e63545c6d269c47ad5
swe2tdream <93404963+swe2tdream@users.noreply.github.com>
2024-08-15 17:16:06 +0300

    Update prcode.js

--- js/prcode.js
+++ js/prcode.js
@@ -1,8 +1,8 @@
 var input = document.querySelector('input');
 
 input.oninput = function() {
-	if (input.value === "0111") {
-		document.cookie = "id=0111; max-age=86400"
+	if (input.value === "2802") {
+		document.cookie = "id=2802; max-age=86400"
 		window.location.href = "learn/labms/profile.html";
 	}
 }
